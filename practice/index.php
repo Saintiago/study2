20336496ef8f23b9182e83d881c09aa81ec9275c
Saintiago <techpriest2007@yandex.ru>
2015-04-13 22:31:33 +0300

    Практика Web 13 апреля

---
+++ practice/index.php
@@ -0,0 +1,11 @@
+<?php
+  require_once("include/common.inc.php");
+  
+  $students = array
+  (
+    array("name" => "Alex", "age" => "20"),
+    array("name" => "Tanya", "age" => "21")
+  );
+  
+  $vars = array("students" => $students);
+  echo BuildPage("students.html", $vars);
