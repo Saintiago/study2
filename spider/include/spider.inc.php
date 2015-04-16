fb95ed3f6fe1aa6213e5efc81523a280c73adf45
Saintiago <techpriest2007@yandex.ru>
2015-04-17 00:37:31 +0300

    Поправил форматирование

--- spider/include/spider.inc.php
+++ spider/include/spider.inc.php
@@ -1,43 +1,44 @@
 <?php
-  require_once('include/phpQuery-onefile.php');
-    
-  function GetInitialLinks($html)
-  {
-    $doc = phpQuery::newDocument($html); 
-    $contents = pq('.toctree-wrapper')->find('a');
-    $arLinks = array();
-    foreach($contents as $a) {
+    require_once('include/phpQuery-onefile.php');
+      
+    function GetInitialLinks($html)
+    {
+        $doc = phpQuery::newDocument($html); 
+        $contents = pq('.toctree-wrapper')->find('a');
+        $arLinks = array();
+        foreach($contents as $a) 
+        {
             $href = pq($a)->attr('href');
             if (strpos($href, '#') !== false)
             {
-              $arHref = explode('#', $href);
-              $href = $arHref[0];
+                $arHref = explode('#', $href);
+                $href = $arHref[0];
             }
             $arLinks[] = $href;
+        }
+        return array_unique($arLinks);
     }
-    return array_unique($arLinks);
-  }
-  
-  function GetArticleText($html)
-  {
-    $doc = phpQuery::newDocument($html); 
-    return pq("div.section")->html();
-  }
-  
-  function GetPageHtml($url)
-  {
-    $ch = curl_init();
-    $timeout = 5;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $html = curl_exec($ch);
-    $info = curl_getinfo($ch);
-    if(curl_errno($ch) || ($info["http_code"] != 200))
+    
+    function GetArticleText($html)
     {
-      $html = false;
+        $doc = phpQuery::newDocument($html); 
+        return pq("div.section")->html();
     }
-    curl_close($ch);
-    return $html;
-  }
+    
+    function GetPageHtml($url)
+    {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $html = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        if(curl_errno($ch) || ($info["http_code"] != 200))
+        {
+            $html = false;
+        }
+        curl_close($ch);
+        return $html;
+    }
--- spider/index.php
+++ spider/index.php
@@ -4,40 +4,40 @@
  * и выводит статьи на экран
  */
 
-  require_once("include/spider.inc.php");
-  
-  $working_path = "https://docs.python.org/3.3/reference/";
-  $arErrors = array();
-  
-  $html = GetPageHtml($working_path . "index.html");
-  if ($html !== false)
-  {
-    $arLinks = GetInitialLinks($html);
-  
-    foreach ($arLinks as $link)
+    require_once("include/spider.inc.php");
+    
+    $working_path = "https://docs.python.org/3.3/reference/";
+    $arErrors = array();
+    
+    $html = GetPageHtml($working_path . "index.html");
+    if ($html !== false)
     {
-      $html = GetPageHtml($working_path . $link);
-      if ($html !== false)
-      {
-        $text .= GetArticleText($html) . "<br/>";
-      }
-      else
-      {
-        $arErrors[] = "Error getting page: " . $working_path . $link;
-      }
+        $arLinks = GetInitialLinks($html);
+    
+        foreach ($arLinks as $link)
+        {
+            $html = GetPageHtml($working_path . $link);
+            if ($html !== false)
+            {
+              $text .= GetArticleText($html) . "<br/>";
+            }
+            else
+            {
+              $arErrors[] = "Error getting page: " . $working_path . $link;
+            }
+        }
     }
-  }
-  else
-  {
-    $arErrors[] = "Error getting contents page (" . $working_path . "index.html )";
-  }
-  
-  header("Content-Type: text/plain; charset=UTF-8");
-
-  if (count($arErrors) > 0)
-  {
-    echo implode("\n", $arErrors);
-    echo "\n\n";
-  }
-   
-  echo strip_tags($text);
+    else
+    {
+        $arErrors[] = "Error getting contents page ( " . $working_path . "index.html )";
+    }
+    
+    header("Content-Type: text/plain; charset=UTF-8");
+    
+    if (count($arErrors) > 0)
+    {
+        echo implode("\n", $arErrors);
+        echo "\n\n";
+    }
+       
+    echo strip_tags($text);
