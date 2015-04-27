7b0e6fbaa71a146c2df4143aba9dbe5de76108f7
Saintiago <techpriest2007@yandex.ru>
2015-04-27 21:02:52 +0300

    Правки

--- lab3/template_c/8b6f5a6362ad18c5a3cea0f8f2a6ec3c1d654569.file.canvas.html.php
+++ lab3/template_c/8b6f5a6362ad18c5a3cea0f8f2a6ec3c1d654569.file.canvas.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 19:28:20
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 19:54:26
          compiled from "C:\web\server2go\htdocs\lab3\template\canvas.html" */ ?>
 <?php /*%%SmartyHeaderCode:9121553e71b4f24b42-99193622%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '8b6f5a6362ad18c5a3cea0f8f2a6ec3c1d654569' => 
     array (
       0 => 'C:\\web\\server2go\\htdocs\\lab3\\template\\canvas.html',
-      1 => 1430155543,
+      1 => 1430156888,
       2 => 'file',
     ),
   ),
@@ -15,9 +15,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.21-dev',
   'unifunc' => 'content_553e71b5027f98_57414201',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_553e71b5027f98_57414201')) {function content_553e71b5027f98_57414201($_smarty_tpl) {?><h1 class="title">Тег &lt;canvas&gt;</h1>﻿
 <div id="like">
@@ -66,10 +66,12 @@ $_valid = $_smarty_tpl->decodeProperties(array (
               </tr>
             </table>
             <h3>Описание</h3>
-            <p>Создает область, в которой при помощи JavaScript можно рисовать
-            разные объекты, выводить изображения, трансформировать их и менять
-            свойства. При помощи тега <span class="tag">&lt;canvas&gt;</span>
-            можно создавать рисунки, анимацию, игры и др.</p>
+            <p>
+              Создает область, в которой при помощи JavaScript можно рисовать
+              разные объекты, выводить изображения, трансформировать их и менять
+              свойства. При помощи тега <span class="tag">&lt;canvas&gt;</span>
+              можно создавать рисунки, анимацию, игры и др.
+            </p>
             <h3>Синтаксис</h3>
             <pre>
                 <code class="no-buttons xml">
@@ -79,22 +81,37 @@ $_valid = $_smarty_tpl->decodeProperties(array (
             </pre>
             <h3>Атрибуты</h3>
             <dl class="param">
-              <dt>height</dt>
-              <dd>Задает высоту холста. По умолчанию 300 пикселов.</dd>
-              <dt>width</dt>
-              <dd>Задает ширину холста. По умолчанию 150 пикселов.</dd>
+              <dt>
+                height
+              </dt>
+              <dd>
+                Задает высоту холста. По умолчанию 300 пикселов.
+              </dd>
+              <dt>
+                width
+              </dt>
+              <dd>
+                Задает ширину холста. По умолчанию 150 пикселов.
+              </dd>
             </dl>
-            <p class="more">Также для этого тега доступны <a href=
-            "http://htmlbook.ru/html/attr/common">универсальные атрибуты</a> и
-            <a href="http://htmlbook.ru/html/attr/event">события</a>.</p>
+            <p class="more">
+              Также для этого тега доступны <a href=
+              "http://htmlbook.ru/html/attr/common">универсальные атрибуты</a> и <a href="http://htmlbook.ru/html/attr/event">события</a>.
+            </p>
             <h3>Закрывающий тег</h3>
-            <p>Обязателен.</p>
-            <p class="exampleTitle">Пример</p>
-            <p class="example-support"><span class=
-            "html yes">HTML5</span><span class="no">IE 8</span><span class=
-            "yes">IE 9</span><span class="yes">Cr</span><span class=
-            "yes">Op</span><span class="yes">Sa</span><span class=
-            "yes">Fx</span></p>
+            <p>
+              Обязателен.
+            </p>
+            <p class="exampleTitle">
+              Пример
+            </p>
+            <p class="example-support">
+              <span class=
+              "html yes">HTML5</span><span class="no">IE 8</span><span class=
+              "yes">IE 9</span><span class="yes">Cr</span><span class=
+              "yes">Op</span><span class="yes">Sa</span><span class=
+              "yes">Fx</span>
+            </p>
             <pre>
 <code>&lt;!DOCTYPE html&gt;
 &lt;html&gt;
@@ -146,19 +163,24 @@ $_valid = $_smarty_tpl->decodeProperties(array (
  &lt;/body&gt;
 &lt;/html&gt;</code>
 </pre>
-            <p>Результат примера в браузере Opera показан на рис.&nbsp;1.</p>
+            <p>
+              Результат примера в браузере Opera показан на рис.&nbsp;1.
+            </p>
             <p class="fig"><img alt=
-            "Вывод рисунка с помощью тега &lt;canvas&gt;" height="299" src=
-            "http://htmlbook.ru/files/images/html/tag_canvas.png" width=
-            "400"></p>
-            <p class="figsign">Рис. 1. Вывод рисунка с помощью тега
-            &lt;canvas&gt;</p>
+              "Вывод рисунка с помощью тега &lt;canvas&gt;" height="299" src=
+              "http://htmlbook.ru/files/images/html/tag_canvas.png" width=
+              "400">
+            </p>
+            <p class="figsign">
+              Рис. 1. Вывод рисунка с помощью тега
+              &lt;canvas&gt;
+            </p>
           </div>
         </div>
       </div>
       <div class=
       "field field-name-taxonomy-vocabulary-10 field-type-taxonomy-term-reference field-label-hidden">
-      <div class="field-items">
+        <div class="field-items">
           <div class="field-item even">
             <a href="/html/type/html5">HTML5</a>
           </div>
--- lab3/template_c/96d0ed38a442dbed56586e052f1f6f377178cd8d.file.footer.html.php
+++ lab3/template_c/96d0ed38a442dbed56586e052f1f6f377178cd8d.file.footer.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 18:32:59
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 20:00:46
          compiled from "C:\web\server2go\htdocs\lab3\template\footer.html" */ ?>
 <?php /*%%SmartyHeaderCode:6114553e5ec2578236-79102897%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '96d0ed38a442dbed56586e052f1f6f377178cd8d' => 
     array (
       0 => 'C:\\web\\server2go\\htdocs\\lab3\\template\\footer.html',
-      1 => 1430152358,
+      1 => 1430157610,
       2 => 'file',
     ),
   ),
@@ -19,52 +19,108 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'unifunc' => 'content_553e5ec257ee72_89097762',
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_553e5ec257ee72_89097762')) {function content_553e5ec257ee72_89097762($_smarty_tpl) {?>        </div>
-    </div>
-    
-    <div class="footer">
-        <div class="section">
-            <h4>О сайте</h4>
-            <p><a href="/help">Помощь</a></p>
-            <p><a href="/about/copy">Копирование материалов</a></p>
-            <p><a href="/about/error">Борьба с ошибками</a></p>
-            <p><a href="/about/tech">Технологии</a></p>
-            <p><a href="/about/search">Поисковый плагин</a></p>
-        </div>
-        <div class="section">
-            <h4>Основные разделы</h4>
-            <p><a href="/content">Статьи</a></p>
-            <p><a href="/blog">Блог</a></p>
-            <p><a href="/practical">Практикум</a></p>
-            <p><a href="/books">Цифровые книги</a></p>
-            <p><a href="/test">Тесты</a></p>
-            <p><a href="http://forum.htmlbook.ru" rel="nofollow">Форум</a></p>
-        </div>
-        <div class="section">
-            <h4>HTML</h4>
-            <p><a href="/samhtml">Самоучитель HTML</a></p>
-            <p><a href="/xhtml">XHTML</a></p>
-            <p><a href="/html">Справочник по HTML</a></p>
-            <p><a href="/html5">HTML5</a></p>
-        </div>
-        <div class="section">
-            <h4>CSS</h4>
-            <p><a href="/samcss">Самоучитель CSS</a></p>
-            <p><a href="/css">Справочник по CSS</a></p>
-            <p><a href="/faq">Рецепты CSS</a></p>
-            <p><a href="/css3">CSS3</a></p>
-        </div>
-        <div class="section">
-            <h4>Сайт</h4>
-            <p><a href="/samlayout">Вёрстка</a></p>
-            <p><a href="/layout">Макеты</a></p>
-            <p><a href="/webserver">Веб-сервер</a></p>
-        </div>
-        <p class="copyright">&copy; 2002–2015 Влад Мержевич, по всем вопросам пишите по адресу vlad@htmlbook.ru</p>
-        <p><a href="http://feeds.feedburner.com/htmlbook" rel="nofollow">Подпишитесь на материалы сайта по RSS</a></p>
-    </div>
+<?php if ($_valid && !is_callable('content_553e5ec257ee72_89097762')) {function content_553e5ec257ee72_89097762($_smarty_tpl) {?></div>
+</div>
 
+<div class="footer">
+  <div class="section">
+    <h4>О сайте</h4>
+    <ul>
+      <li>
+        <a href="/help">Помощь</a>
+      </li>
+      <li>
+        <a href="/about/copy">Копирование материалов</a>
+      </li>
+      <li>
+        <a href="/about/error">Борьба с ошибками</a>
+      </li>
+      <li>
+        <a href="/about/tech">Технологии</a>
+      </li>
+      <li>
+        <a href="/about/search">Поисковый плагин</a>
+      </li>
+    </ul>
+  </div>
+  <div class="section">
+    <h4>Основные разделы</h4>
+    <ul>
+      <li>
+        <a href="/content">Статьи</a>
+      </li>
+      <li>
+        <a href="/blog">Блог</a>
+      </li>
+      <li>
+        <a href="/practical">Практикум</a>
+      </li>
+      <li>
+        <a href="/books">Цифровые книги</a>
+      </li>
+      <li>
+        <a href="/test">Тесты</a>
+      </li>
+      <li>
+        <a href="http://forum.htmlbook.ru" rel="nofollow">Форум</a>
+      </li>
+    </ul>
+  </div>
+  <div class="section">
+    <h4>HTML</h4>
+    <ul>
+      <li>
+        <a href="/samhtml">Самоучитель HTML</a>
+      </li>
+      <li>
+        <a href="/xhtml">XHTML</a>
+      </li>
+      <li>
+        <a href="/html">Справочник по HTML</a>
+      </li>
+      <li>
+        <a href="/html5">HTML5</a>
+      </li>
+    </ul>
+  </div>
+  <div class="section">
+    <h4>CSS</h4>
+    <ul>
+      <li>
+        <a href="/samcss">Самоучитель CSS</a>
+      </li>
+      <li>
+        <a href="/css">Справочник по CSS</a>
+      </li>
+      <li>
+        <a href="/faq">Рецепты CSS</a>
+      </li>
+      <li>
+        <a href="/css3">CSS3</a>
+      </li>
+  </div>
+  <div class="section">
+    <h4>Сайт</h4>
+    <ul>
+      <li>
+        <a href="/samlayout">Вёрстка</a>
+      </li>
+      <li>
+        <a href="/layout">Макеты</a>
+      </li>
+      <li>
+        <a href="/webserver">Веб-сервер</a>
+      </li>
+    </ul>
+  </div>
+  <p class="copyright">
+    &copy; 2002–2015 Влад Мержевич, по всем вопросам пишите по адресу vlad@htmlbook.ru
+  </p>
+  <p>
+    <a href="http://feeds.feedburner.com/htmlbook" rel="nofollow">Подпишитесь на материалы сайта по RSS</a>
+  </p>
+</div>
 
-</body>  
-  
+</body>
+
 </html><?php }} ?>
