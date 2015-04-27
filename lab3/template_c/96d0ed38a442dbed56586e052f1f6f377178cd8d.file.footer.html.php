5065b87933bd075a7b177e35125bd6f90861f2c5
Saintiago <techpriest2007@yandex.ru>
2015-04-27 19:33:47 +0300

    Правка для ие8

--- lab3/template_c/96d0ed38a442dbed56586e052f1f6f377178cd8d.file.footer.html.php
+++ lab3/template_c/96d0ed38a442dbed56586e052f1f6f377178cd8d.file.footer.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 18:14:47
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 18:32:59
          compiled from "C:\web\server2go\htdocs\lab3\template\footer.html" */ ?>
 <?php /*%%SmartyHeaderCode:6114553e5ec2578236-79102897%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '96d0ed38a442dbed56586e052f1f6f377178cd8d' => 
     array (
       0 => 'C:\\web\\server2go\\htdocs\\lab3\\template\\footer.html',
-      1 => 1430151201,
+      1 => 1430152358,
       2 => 'file',
     ),
   ),
@@ -23,15 +23,15 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     </div>
     
     <div class="footer">
-        <section>
+        <div class="section">
             <h4>О сайте</h4>
             <p><a href="/help">Помощь</a></p>
             <p><a href="/about/copy">Копирование материалов</a></p>
             <p><a href="/about/error">Борьба с ошибками</a></p>
             <p><a href="/about/tech">Технологии</a></p>
             <p><a href="/about/search">Поисковый плагин</a></p>
-        </section>
-        <section>
+        </div>
+        <div class="section">
             <h4>Основные разделы</h4>
             <p><a href="/content">Статьи</a></p>
             <p><a href="/blog">Блог</a></p>
@@ -39,27 +39,27 @@ $_valid = $_smarty_tpl->decodeProperties(array (
             <p><a href="/books">Цифровые книги</a></p>
             <p><a href="/test">Тесты</a></p>
             <p><a href="http://forum.htmlbook.ru" rel="nofollow">Форум</a></p>
-        </section>
-        <section>
+        </div>
+        <div class="section">
             <h4>HTML</h4>
             <p><a href="/samhtml">Самоучитель HTML</a></p>
             <p><a href="/xhtml">XHTML</a></p>
             <p><a href="/html">Справочник по HTML</a></p>
             <p><a href="/html5">HTML5</a></p>
-        </section>
-        <section>
+        </div>
+        <div class="section">
             <h4>CSS</h4>
             <p><a href="/samcss">Самоучитель CSS</a></p>
             <p><a href="/css">Справочник по CSS</a></p>
             <p><a href="/faq">Рецепты CSS</a></p>
             <p><a href="/css3">CSS3</a></p>
-        </section>
-        <section>
+        </div>
+        <div class="section">
             <h4>Сайт</h4>
             <p><a href="/samlayout">Вёрстка</a></p>
             <p><a href="/layout">Макеты</a></p>
             <p><a href="/webserver">Веб-сервер</a></p>
-        </section>
+        </div>
         <p class="copyright">&copy; 2002–2015 Влад Мержевич, по всем вопросам пишите по адресу vlad@htmlbook.ru</p>
         <p><a href="http://feeds.feedburner.com/htmlbook" rel="nofollow">Подпишитесь на материалы сайта по RSS</a></p>
     </div>
